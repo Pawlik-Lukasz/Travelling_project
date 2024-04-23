1dbf8a96b08311e7eb4521f96ad8d03d0dcbe47b
Pawlik-Lukasz <Pazur123@onet.pl>
2024-04-23 17:06:39 +0200

    File responsible for google sheets

---
+++ data_manager.py
@@ -0,0 +1,38 @@
+import os
+
+import requests
+import os
+from dotenv import find_dotenv
+
+dotenv_path = find_dotenv()
+
+
+class DataManager:
+
+    def __init__(self):
+        self.sheety_data = {}
+        self.sheety_API = os.getenv('SHEETY_API')
+
+    def get_data(self):
+        headers = {
+            "Authorization": "Bearer iaywvdiajsdoia"
+        }
+        sheety_response = requests.get(url=self.sheety_API, headers=headers)
+
+        self.sheety_data = sheety_response.json()["flightProject"]
+        return self.sheety_data
+
+    def put_data(self):
+        for line in self.sheety_data:
+            id = line["id"]
+            headers = {
+                "Authorization": "Bearer iaywvdiajsdoia"
+            }
+            body = {
+                "flightProject": {
+                    "iataCode": line["iataCode"]
+                }
+            }
+            sheety_put_API = f"{self.sheety_API}/{id}"
+            sheety_put_response = requests.put(url=sheety_put_API, json=body, headers=headers)
+            print("response.text= ", sheety_put_response.text)
